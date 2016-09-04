fe3546313446236997de1f560fc46b403be40b17
adam russell <adam@backpackhealth.com>
2016-09-04 15:47:20 -0400

    travis attempt 1

--- test/App.spec.js
+++ test/App.spec.js
@@ -6,8 +6,17 @@ import Login from '../app/Login'
 import { shallow, mount } from 'enzyme'
 
 describe('<Login />', () => {
-  it('should render the brand', () => {
+  it('should render the title', () => {
     const wrapper = shallow(<Login />)
     expect(wrapper.contains(<h1>Login Page</h1>)).to.be.true
+  })
+})
+
+describe('Useless tests', () => {
+  it('should do basic addition', () => {
+    expect(1+3).to.equal(4)
+  })
+  it('should do array lengths', () => {
+    expect([2,2,2,2].length).to.equal(4)
   })
 })
